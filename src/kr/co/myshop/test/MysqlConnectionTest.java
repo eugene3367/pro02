d3476bdf5a2b8b5be5704cf7447321da2aa1d3b8
eugene3367 <dbwls2518@naver.com>
2022-11-01 18:33:07 +0900

    pro022

--- src/kr/co/myshop/test/MysqlConnectionTest.java
+++ src/kr/co/myshop/test/MysqlConnectionTest.java
@@ -1,10 +1,14 @@
 package kr.co.myshop.test;
 
 import static org.junit.Assert.*;
+
+import java.util.logging.Logger;
+
 import org.junit.Test;
 
 public class MysqlConnectionTest {
-
+	private final static Logger log = Logger.getGlobal();
+	private final static String DRIVER = "";
 	@Test
 	public void test() {
 		fail("Not yet implemented");
